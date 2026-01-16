867a1fdbc2c387431f7abf615718610b589feaa4
kimtaeha606 <sinjaesang6@gmail.com>
2026-01-16 13:35:50 +0900

    Update GameManager.cs

--- idle_mine/Assets/Scripts/A/GameManager.cs
+++ idle_mine/Assets/Scripts/A/GameManager.cs
@@ -1,6 +1,26 @@
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    
+    private void OnEnable()
+    {
+        // 이벤트 구독
+        GameSignals.PickUpgradeRequested += OnPickUpgradeRequested;
+    }
+
+    private void OnDisable()
+    {
+        // 이벤트 해제 (필수)
+        GameSignals.PickUpgradeRequested -= OnPickUpgradeRequested;
+    }
+
+    private void OnPickUpgradeRequested()
+    {
+        // 여기서 업그레이드 흐름을 시작
+        Debug.Log("Pick 업그레이드 요청 수신");
+
+        // 예:
+        // pickManager.TryUpgrade();
+        // economy.TrySpend();
+    }
 }
